42a10e89232d4d9311872a0e07c56742dd0ed8fa
SreeGD <Sreenivas.Mallipeddi@gmail.com>
2026-05-22 20:43:28 +0530

    Fix WisdomLib integration ordering in publish.sh

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
      - cache-first for static assets (CSS / JS / JSON / SVG / fonts)
      - precache the app shell on install so first offline visit still works */
 
-const VERSION = "v20260522150634";
+const VERSION = "v20260522151245";
 const CACHE = "foliocorpus-" + VERSION;
 const SHELL = [
     "./",
