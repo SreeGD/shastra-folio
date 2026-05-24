13f7128837d68d593ce9ce4a290d41ac51b76256
SreeGD <Sreenivas.Mallipeddi@gmail.com>
2026-05-24 16:40:57 +0530

    Batch D: 5 more inline readers (Bhakti-rasāyana / Stavāvalī / Krishna-bhāvanāmṛta / Brāhmaṇa-and-Vaiṣṇava / Notes on Bhāgavata)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
      - cache-first for static assets (CSS / JS / JSON / SVG / fonts)
      - precache the app shell on install so first offline visit still works */
 
-const VERSION = "v20260523175141";
+const VERSION = "v20260524111013";
 const CACHE = "foliocorpus-" + VERSION;
 const SHELL = [
     "./",
