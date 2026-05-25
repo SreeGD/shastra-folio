86fedff7534455c6633fef0a7b9acd279a75f1ce
SreeGD <Sreenivas.Mallipeddi@gmail.com>
2026-05-25 12:57:59 +0530

    BG recitation: ship only HG Rādhā-Gopīnātha Prabhu

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
      - cache-first for static assets (CSS / JS / JSON / SVG / fonts)
      - precache the app shell on install so first offline visit still works */
 
-const VERSION = "v20260525070433";
+const VERSION = "v20260525072719";
 const CACHE = "foliocorpus-" + VERSION;
 const SHELL = [
     "./",
