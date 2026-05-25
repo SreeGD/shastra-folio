2f1a1e91ca8e0ba9cf15c89aa516cd8786cf0cc5
SreeGD <Sreenivas.Mallipeddi@gmail.com>
2026-05-25 12:19:10 +0530

    Fix BG collapse: drop fc-section-label from <summary> elements

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
      - cache-first for static assets (CSS / JS / JSON / SVG / fonts)
      - precache the app shell on install so first offline visit still works */
 
-const VERSION = "v20260525063329";
+const VERSION = "v20260525064830";
 const CACHE = "foliocorpus-" + VERSION;
 const SHELL = [
     "./",
