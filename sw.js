db79c85ee675f5e6762837f33815067dcc882a1b
SreeGD <Sreenivas.Mallipeddi@gmail.com>
2026-06-11 22:22:38 +0530

    republish: 2026-06-11 22:20:45

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
      - cache-first for static assets (CSS / JS / JSON / SVG / fonts)
      - precache the app shell on install so first offline visit still works */
 
-const VERSION = "v20260611164113";
+const VERSION = "v20260611165145";
 const CACHE = "foliocorpus-" + VERSION;
 const SHELL = [
     "./",
